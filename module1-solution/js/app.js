77a88b6e8e5db0368b0ea95d8b2108f21bcf6e05
DanielNekrasov <danielnekrasov@yandex.ru>
2016-09-14 18:43:29 +0500

    module1_solution

---
+++ module1-solution/js/app.js
@@ -0,0 +1,47 @@
+//var arrayOfStrings = stringToSplit.split(separator);
+(function () {
+  'use strict'
+angular.module("LunchCheck",[])
+  
+  .controller("LunchCheckController", LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope){
+    $scope.Checker = function(){
+      var NumberOfItems = 0;
+
+      if ($scope.menu){
+        var ArrayOfitems = $scope.menu.split(',');
+        for (var i = 0; i<ArrayOfitems.length; i++){
+          if (ArrayOfitems[i]){
+            NumberOfItems++;
+          }
+        };
+
+       if(NumberOfItems<=3){
+          $scope.message="Enjoy!";
+          $scope.color="greenText";
+        } else {
+          $scope.message="Too much!";
+          $scope.color="greenText";
+        }
+      }
+
+      else{
+        $scope.message="Please enter data first";  
+        $scope.color="redText";
+      }
+     
+
+ 
+    }
+
+
+  }
+
+
+
+
+})();
+
